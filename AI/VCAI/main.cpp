b6661de23f4395f8450cca8d21f4d623b536c70b
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2014-02-23 18:04:34 +0400

    Make fully static build possible.

--- AI/VCAI/main.cpp
+++ AI/VCAI/main.cpp
@@ -5,19 +5,19 @@
 #define strcpy_s(a, b, c) strncpy(a, c, b)
 #endif
 
-const char *g_cszAiName = "VCAI";
+const char *VCAI_g_cszAiName = "VCAI";
 
-extern "C" DLL_EXPORT int GetGlobalAiVersion()
+extern "C" DLL_EXPORT int VCAI_GetGlobalAiVersion()
 {
 	return AI_INTERFACE_VER;
 }
 
-extern "C" DLL_EXPORT void GetAiName(char* name)
+extern "C" DLL_EXPORT void VCAI_GetAiName(char* name)
 {
-	strcpy_s(name, strlen(g_cszAiName) + 1, g_cszAiName);
+	strcpy_s(name, strlen(VCAI_g_cszAiName) + 1, VCAI_g_cszAiName);
 }
 
-extern "C" DLL_EXPORT void GetNewAI(shared_ptr<CGlobalAI> &out)
+extern "C" DLL_EXPORT void VCAI_GetNewAI(shared_ptr<CGlobalAI> &out)
 {
 	out = make_shared<VCAI>();
 }
